7543b8ad0b28d4ff3f44f29a9c53f96faafa79d8
daxter412 <tony_nerso@hotmail.com>
2015-09-14 18:11:14 -0500

    Update For.cpp

--- For.cpp
+++ For.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//http://www.taringa.net/post/hazlo-tu-mismo/18784406/Como-practicar-hacking-sin-ir-preso-Pt-1.html
 int main ()
 {
 	for ( int x=0 ; x<=10 ; x++) {
